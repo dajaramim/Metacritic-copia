6a9bee5c45bca617f2fbe51d45d4d5aedee671a7
Daniel Jaramillo <da.jaramim@gmail.com>
2022-12-20 17:16:55 -0300

    barra de navegación lista

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const sass = require('gulp-sass')(require('sass'));
 
 function css(done) {
     src('src/scss/app.scss')
-    .pipe( sass() )
+    .pipe( sass({outputStyle: 'expanded'}) )
     .pipe( dest('build/css'))
 
     done();
 }
 
 function dev() { //la función no se puede llamar watch, no pueden haber dos funciones llamadas igual.
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
 }
 
 exports.css = css;
